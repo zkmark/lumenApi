c5371f73ec65e71d21e71cd72a539e2cefec06fc
mark <xeneas@hotmail.com>
2016-10-15 13:22:37 -0500

    3.2 recursos con controladores CursoEstudiante y ProfesorCurso

--- app/Http/Controllers/CursoEstudianteController.php
+++ app/Http/Controllers/CursoEstudianteController.php
@@ -1,10 +1,22 @@
-<?php namespace App\Http\Controllers;
+<?php 
+namespace App\Http\Controllers;
+
+use App\Curso;
+
 class CursoEstudianteController extends Controller
 {
-	public function index()
-	{
-		return 'desde index en cursoestudiantecontroller';
+	public function index($curso_id){
+		
+		$curso = Curso::find($curso_id);
+		
+		//Si existe
+		if($curso){
+			$estudiantes = $curso->estudiantes;
+			return $this->crearRespuesta($estudiantes, 200);
+		}
+		return $this->crearRespuestaError('No se puede encontrar un curso con el id dado', 404);
 	}
+
 	public function store()
 	{
 		return 'desde store en cursoestudiantecontroller';
--- app/Http/Controllers/ProfesorCursoController.php
+++ app/Http/Controllers/ProfesorCursoController.php
@@ -1,10 +1,21 @@
-<?php namespace App\Http\Controllers;
-class ProfesorCursoController extends Controller
-{
-	public function index()
-	{
-		return 'desde index en profesorcursocontroller';
+<?php 
+namespace App\Http\Controllers;
+
+use App\Profesor;
+
+class ProfesorCursoController extends Controller{
+
+	public function index($profesor_id){
+		$profesor = Profesor::find($profesor_id);
+		if($profesor){
+
+			$cursos = $profesor->cursos;
+			return $this->crearRespuesta($cursos, 200);
+			
+		}
+		return $this->crearRespuestaError('No se puede encontrar un profesor con el id dado', 404);
 	}
+
 	public function update()
 	{
 		return 'desde update en profesorcursocontroller';
